98f5f11297843adcbb0dfd2a3a8956da2669ea9b
aki <akihiko00asumi@gmail.com>
2019-06-09 21:13:04 +0900

    add API and test

--- main.go
+++ main.go
@@ -1,19 +1,116 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"sort"
+
+	"time"
 
 	"github.com/gin-gonic/gin"
+
+	"strconv"
 )
 
-func setupRouter() *gin.Engine {
-	r := gin.Default()
+type Event struct {
+	Deadline time.Time
+	Title    string
+	Memo     string
+}
 
-	r.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"status": "OK",
+var table map[int]Event
+var lastid int
+
+func registerEvent(ctx *gin.Context) {
+	var body struct {
+		Deadline string `json:"deadline"`
+		Title    string `json:"title"`
+		Memo     string `json:"memo"`
+	}
+
+	err := ctx.ShouldBindJSON(&body)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"status":  "failure",
+			"message": "invalid request format",
 		})
+		return
+	}
+
+	t, err := time.Parse(time.RFC3339, body.Deadline)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"status":  "failure",
+			"message": "invalid date format",
+		})
+		return
+	}
+
+	event := Event{t, body.Title, body.Memo}
+	lastid++
+	table[lastid] = event
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"status":  "success",
+		"message": "registered",
+		"id":      lastid,
 	})
+}
+
+func getAllEvents(ctx *gin.Context) {
+	type Response struct {
+		ID       int    `json:"id"`
+		Deadline string `json:"deadline"`
+		Title    string `json:"title"`
+		Memo     string `json:"memo"`
+	}
+
+	keys := []int{}
+	for id := range table {
+		keys = append(keys, id)
+	}
+	sort.Ints(keys)
+	entries := []Response{}
+	for _, k := range keys {
+		y := table[k]
+		e := Response{k, y.Deadline.Format(time.RFC3339), y.Title, y.Memo}
+		entries = append(entries, e)
+	}
+
+	json, err := json.Marshal(entries)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"status":  "failure",
+			"message": "internal server error",
+		})
+		return
+	}
+
+	ctx.String(http.StatusOK, string(json))
+}
+
+func getEvent(ctx *gin.Context) {
+	id, _ := strconv.Atoi(ctx.Param("id"))
+	if event, ok := table[id]; ok {
+		ctx.JSON(http.StatusOK, gin.H{
+			"id":       id,
+			"deadline": event.Deadline.Format(time.RFC3339),
+			"title":    event.Title,
+			"memo":     event.Memo,
+		})
+	} else {
+		ctx.String(http.StatusNotFound, "")
+	}
+}
+
+func setupRouter() *gin.Engine {
+	table = make(map[int]Event)
+	lastid = 0
+
+	r := gin.Default()
+	r.POST("/api/v1/event", registerEvent)
+	r.GET("/api/v1/event", getAllEvents)
+	r.GET("/api/v1/event/:id", getEvent)
 
 	return r
 }
--- server_test.go
+++ server_test.go
@@ -1,26 +1,161 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestServer(t *testing.T) {
+type Response struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+	ID      *int   `json:"id,omitempty"`
+}
+
+type RequestEvent struct {
+	ID       *int   `json:"id,omitempty"`
+	Deadline string `json:"deadline"`
+	Title    string `json:"title"`
+	Memo     string `json:"memo"`
+}
+
+func EqualP(t *testing.T, expected, actual interface{}, msgAndArgs ...interface{}) {
+	if assert.Equal(t, expected, actual, msgAndArgs) == false {
+		assert.FailNow(t, "FailNow!", msgAndArgs)
+	}
+}
+
+func EqualEvent(t *testing.T, expectedID int, expected, actual RequestEvent) {
+	EqualP(t, expected.Deadline, actual.Deadline)
+	EqualP(t, expected.Title, actual.Title)
+	EqualP(t, expected.Memo, actual.Memo)
+	EqualP(t, expectedID, *actual.ID)
+}
+
+func PostEvent(t *testing.T, r *gin.Engine, ev RequestEvent) int {
+	w := httptest.NewRecorder()
+	d, _ := json.Marshal(ev)
+	req, _ := http.NewRequest("POST", "/api/v1/event", bytes.NewReader(d))
+	r.ServeHTTP(w, req)
+
+	EqualP(t, 200, w.Code)
+	body := Response{}
+	json.Unmarshal(w.Body.Bytes(), &body)
+	EqualP(t, "success", body.Status)
+	EqualP(t, "registered", body.Message)
+	t.Logf("assigned event id: %v\n", *body.ID)
+	return *body.ID
+}
+
+func GetEvent(t *testing.T, r *gin.Engine, id int) RequestEvent {
+	w := httptest.NewRecorder()
+	t.Logf("request to %v\n", "/api/v1/event/"+strconv.Itoa(id))
+	req, _ := http.NewRequest("GET", "/api/v1/event/"+strconv.Itoa(id), nil)
+	r.ServeHTTP(w, req)
+
+	EqualP(t, 200, w.Code)
+	body := RequestEvent{}
+	json.Unmarshal(w.Body.Bytes(), &body)
+	return body
+}
+
+func GetEvents(t *testing.T, r *gin.Engine) []RequestEvent {
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/event", nil)
+	r.ServeHTTP(w, req)
+
+	EqualP(t, 200, w.Code)
+	body := []RequestEvent{}
+	json.Unmarshal(w.Body.Bytes(), &body)
+
+	t.Logf("GET all events: %v\n", w.Body.String())
+	return body
+}
+
+func Test1(t *testing.T) {
 	r := setupRouter()
 
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/hello", nil)
+	d, _ := json.Marshal(RequestEvent{nil, "2019-06-11T14:00:00+09:00", "レポート提出", ""})
+	req, _ := http.NewRequest("POST", "/api/v1/event", bytes.NewReader(d))
 	r.ServeHTTP(w, req)
 
-	assert.Equal(t, 200, w.Code)
+	EqualP(t, 200, w.Code)
 
-	var body struct {
-		Status string `json:"status"`
-	}
+	body := Response{}
 	json.Unmarshal(w.Body.Bytes(), &body)
-	assert.Equal(t, "OK", body.Status)
+	EqualP(t, "success", body.Status)
+	EqualP(t, "registered", body.Message)
+	t.Logf("assigned event id: %v\n", *body.ID)
+}
+
+func Test2(t *testing.T) {
+	r := setupRouter()
+
+	w := httptest.NewRecorder()
+	d, _ := json.Marshal(RequestEvent{nil, "2019/06/11T14:00:00+09:00", "レポート提出", ""})
+	req, _ := http.NewRequest("POST", "/api/v1/event", bytes.NewReader(d))
+	r.ServeHTTP(w, req)
+
+	EqualP(t, 400, w.Code)
+
+	body := Response{}
+	json.Unmarshal(w.Body.Bytes(), &body)
+	EqualP(t, "failure", body.Status)
+	EqualP(t, "invalid date format", body.Message)
+}
+
+func Test3(t *testing.T) {
+	r := setupRouter()
+
+	requestEvent := RequestEvent{nil, "2019-06-11T14:00:00+09:00", "レポート提出", "memomemo"}
+	id := PostEvent(t, r, requestEvent)
+
+	body2 := GetEvent(t, r, id)
+	EqualEvent(t, id, requestEvent, body2)
+}
+
+func Test4(t *testing.T) {
+	r := setupRouter()
+
+	ev := RequestEvent{nil, "2019-06-11T14:00:00+09:00", "レポート提出", "memomemo"}
+	id := PostEvent(t, r, ev)
+
+	ev2 := RequestEvent{nil, "2019-06-12T14:00:00+09:00", "レポート提出2", "memomemo2"}
+	id2 := PostEvent(t, r, ev2)
+
+	evR := GetEvent(t, r, id)
+	EqualEvent(t, id, ev, evR)
+
+	ev2R := GetEvent(t, r, id2)
+	EqualEvent(t, id2, ev2, ev2R)
+}
+
+func Test5(t *testing.T) {
+	r := setupRouter()
+
+	ev := RequestEvent{nil, "2019-06-11T14:00:00+09:00", "レポート提出", "memomemo"}
+	id := PostEvent(t, r, ev)
+
+	ev2 := RequestEvent{nil, "2019-06-12T14:00:00+09:00", "レポート提出2", "memomemo2"}
+	id2 := PostEvent(t, r, ev2)
+
+	ev3 := RequestEvent{nil, "2019-06-13T14:00:00+09:00", "レポート提出3", "memomemo3"}
+	id3 := PostEvent(t, r, ev3)
+
+	ev4 := RequestEvent{nil, "2019-06-14T14:00:00+09:00", "レポート提出3", "memomemo4"}
+	id4 := PostEvent(t, r, ev4)
+
+	evs := GetEvents(t, r)
+	EqualP(t, 4, len(evs))
+	EqualEvent(t, id, ev, evs[0])
+	EqualEvent(t, id2, ev2, evs[1])
+	EqualEvent(t, id3, ev3, evs[2])
+	EqualEvent(t, id4, ev4, evs[3])
 }
